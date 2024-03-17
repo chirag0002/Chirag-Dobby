d3b2fa57afc34d9ad33d302008e9abe7965a44ed
Chirag Varshney <87241050+chirag0002@users.noreply.github.com>
2024-03-18 02:00:02 +0530

    prod

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = 'http://localhost:8080/api/v1'
+const BASE_URL = 'https://dobby-api.onrender.com/api/v1'
 
 export const API = axios.create({
     baseURL: BASE_URL
